124abf049e25b59b5dd641c8f9089eb91585c848
desmovalvo <desmovalvo@gmail.com>
2014-04-03 16:53:26 +0200

    Publisher cleaned

--- tpublisher2.py
+++ tpublisher2.py
@@ -12,65 +12,28 @@ from termcolor import *
 
 
 def handler(sock, ssap_msg):
-    print "thread>" + ssap_msg
-    print
+    print colored("tpublisher> ", "blue", attrs=["bold"]) + "started a thread"
 
     # socket to the real SIB
     rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     rs.connect((realsib_host, realsib_port))
-    print str(rs)
 
     # forward the message to the real SIB
     if not "<transaction_type>REGISTER</transaction_type>" in ssap_msg:
         rs.send(ssap_msg)
 
-        # receive data from the socket
-        # if ("<transaction_type>UNSUBSCRIBE</transaction_type>" in ssap_msg and "<message_type>REQUEST</message_type>"):
-        #     # parse the ssap message to get the subscription ID and store the relative socket                
-        #     print "Parsing message"
-        #     root = ET.fromstring(ssap_msg)           
-        #     info = {}
-        #     for child in root:
-        #         if child.attrib.has_key("name"):
-        #             k = child.tag + "_" + str(child.attrib["name"])
-        #         else:
-        #             k = child.tag
-        #         info[k] = child.text                    
-                
-        #     # receive ON THE PORT DEDICATED TO THAT SUBSCRIPTION!
-        #     ssap_msg = subs[info["parameter_subscription_id"]].recv(4096)
-        # else:
-        #     ssap_msg = rs.recv(4096)
         if ("<transaction_type>SUBSCRIBE</transaction_type>" in ssap_msg and "<message_type>REQUEST</message_type>"):
             # start a new thread to handle it
             thread.start_new_thread(subscription_handler, (rs, vs))
-            pass
+
         else:
             ssap_msg = rs.recv(4096)
             
             if ssap_msg:
-                print colored("tpublisher>", "red", attrs=["bold"]) + " Received confirm message from the Real Sib"
-    
-                # if not ("<transaction_type>SUBSCRIBE</transaction_type>" in ssap_msg and "<message_type>CONFIRM</message_type>" in ssap_msg):
-                #     rs.close()
-                #     if ("<transaction_type>UNSUBSCRIBE</transaction_type>" in ssap_msg and "<message_type>REQUEST</message_type>"):
-                #         subs[info["parameter_subscription_id"]].close()                
-                # else:
-
-                # parse the ssap message to get the subscription ID and store the relative socket                
-                # print "Parsing message"
-                # root = ET.fromstring(ssap_msg)           
-                # info = {}
-                # for child in root:
-                #     if child.attrib.has_key("name"):
-                #         k = child.tag + "_" + str(child.attrib["name"])
-                #     else:
-                #         k = child.tag
-                #     info[k] = child.text                    
-                # subs[info["parameter_subscription_id"]] = rs
+                print colored("tpublisher>", "blue", attrs=["bold"]) + " Received confirm message from the Real Sib"
                         
                 rs.close()
-                print colored("tpublisher>", "red", attrs=["bold"]) + " Forwarding confirm message to the Virtual Sib"
+                print colored("tpublisher>", "blue", attrs=["bold"]) + " Forwarding confirm message to the Virtual Sib"
                 vs.send(ssap_msg)
 
 
@@ -80,12 +43,10 @@ def subscription_handler(rs, vs):
     while 1:
         ssap_msg = rs.recv(4096)
         if len(ssap_msg) > 1:
-            print colored("tpublisher (sub_handler)>", "red", attrs=["bold"]) + ssap_msg
-            print
+            # forwarding subscription-related message to the virtual sib
+            print colored("tpublisher>", "blue", attrs=["bold"]) + " Forwarding subscription-related message to the Virtual Sib"
             vs.send(ssap_msg)
             
-    pass
-
  
 #main function
 if __name__ == "__main__":
@@ -94,14 +55,16 @@ if __name__ == "__main__":
     node_id = str(uuid.uuid4())
 
     if(len(sys.argv) < 5) :
-        print 'Usage : python publisher.py vsib_hostname vsib_port realsib_hostname realsib_port'
+        print colored("tpublisher> ", "red", attrs=["bold"]) + 'Usage : python publisher.py vsib_hostname vsib_port realsib_hostname realsib_port'
         sys.exit()
      
+    # define hosts data
     vsib_host = sys.argv[1]
     vsib_port = int(sys.argv[2])
     realsib_host = sys.argv[3]
     realsib_port = int(sys.argv[4])
      
+    # socket to the virtual sib
     vs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     vs.settimeout(2)
      
@@ -109,11 +72,10 @@ if __name__ == "__main__":
     try :
         vs.connect((vsib_host, vsib_port))
     except :
-        print 'Unable to connect'
-        sys.exit()
-     
+        print colored("tpublisher> ", "red", attrs=['bold']) + 'Unable to connect to the virtual SIB'
+        sys.exit()    
 
-    print 'Connected to remote host. Sending register request!'
+    print colored("tpublisher> ", "blue", attrs=['bold']) + 'Connected to virtual SIB. Sending register request!'
 
     # building and sending the register request
     space_id = "X"
@@ -135,7 +97,7 @@ if __name__ == "__main__":
             if sock == vs:
                 ssap_msg = sock.recv(4096)
                 if not ssap_msg :
-                    print '\nDisconnected from chat server'
+                    print colored("tpublisher> ", "red", attrs=["bold"]) + 'Disconnected from the virtual SIB'
                     sys.exit()
                 else :
                     thread.start_new_thread(handler, (sock, ssap_msg))
