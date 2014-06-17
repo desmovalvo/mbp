75c39e404d927206d699cdeb82c6c4687c8995a5
pasdeshutter <alessandra.persano@gmail.com>
2014-06-17 14:18:31 +0200

    add/remove sib to/from a multi sib

--- test/sp2b/python/turtle-parser.py
+++ test/sp2b/python/turtle-parser.py
@@ -6,6 +6,7 @@ import rdflib
 import logging
 from SIBLib import *
 from smart_m3.m3_kp_api import *
+import timeit
 
 # logger configuration
 logging.basicConfig(filename="logfile", level=logging.DEBUG)
@@ -15,9 +16,29 @@ logger = logging.getLogger("logger")
 n3file = sys.argv[1]
 sib_ip = sys.argv[2]
 sib_port = int(sys.argv[3])
+virt_sib_ip = sys.argv[4]
+virt_sib_port = int(sys.argv[5])
+num_iter = int(sys.argv[6])
+triple_list = []
+kps = [] 
 
 # join the SIB
-a = SibLib(sib_ip, sib_port)
+rkp = SibLib(sib_ip, sib_port)
+
+#vkp = SibLib(virt_sib_ip, virt_sib_port)
+#kps.append(vkp)
+kps.append(rkp)
+
+#############################################################
+#
+# CLEAN
+#
+#############################################################
+
+print "\nCleaning the sib...\n"
+rkp.remove(Triple(None, None, None))
+print "Triples expected at this step: 0"
+print "Triples at this step: " + str(len(rkp.execute_rdf_query(Triple(None, None, None))))
 
 # creating a Graph instance
 print "Creating a graph instance...",
@@ -29,8 +50,8 @@ print "Parsing the turtle file...",
 res = g.parse('sp2b.n3', format='n3')
 print "OK"
 
-# Printing all the triples:
-counter = 0
+
+# building triple_list:
 for triple in res:
 
     s = []
@@ -46,12 +67,48 @@ for triple in res:
             s.append( bNode(t.toPython()) )
 
     if len(s) == 3:
-        counter += 1
         tr = Triple(s[0], s[1], s[2])
-        print "Inserting the triple " + str(counter) + ":",
-        a.insert(tr)
-        print "ok!"
+        triple_list.append(tr)
 
+
+median_arrays = []
+for client in kps:
+
+    tot = []
+    # insert
+    for it in range(num_iter):   
+
+        print "Iteration: " + str(it)
+    
+        # insert the triples
+        end = timeit.timeit(lambda: client.insert(triple_list), number=1)
+        # append the time
+        tot.append(end)
+
+        # clean sib
+#        client.remove(Triple(None, None, None))
+
+    # sort the array and pick the median value
+    tot.sort()
+    if len(tot)%2 == 0:
+        median = (tot[len(tot)/2] + tot[len(tot)/2-1]) / 2
+    else:
+        median = tot[len(tot)/2]     
+            
+    median_arrays.append([median * 1000])
+
+
+for i in median_arrays:
+    print "MEDIAN ARRAY: " + str(i)
+bar_chart = pygal.Bar()
+# chart = pygal.StackedLine(fill=True, interpolate='cubic', style=LightGreenStyle)
+bar_chart.add('Real SIB', median_arrays[0])
+bar_chart.add('Virtual SIB', median_arrays[1])
+bar_chart.render_to_file('multiple_sub_insert.svg')
+
+
+
+    
         # except socket.error:
         #     print "SOCKET ERROR"
         #     sys.exit()
---
+++ test/timeit-variableinsert-fixedsubscriptions.py
@@ -0,0 +1,187 @@
+import sys
+import timeit
+import pygal
+from lib.SIBLib import *
+from termcolor import *
+from smart_m3.m3_kp_api import *
+import time
+
+# namespace
+ns = "http://ns#"
+
+# variables
+step = 100
+min = 0
+median_arrays = []
+kp_sub = []
+subs = []
+
+# define hosts data
+sib_ip = sys.argv[1]
+sib_port = int(sys.argv[2])
+virtual_sib_ip = sys.argv[3]
+virtual_sib_port = int(sys.argv[4])
+num_sub = int(sys.argv[5])
+num_iter = int(sys.argv[6])
+max_ins = int(sys.argv[7])
+sub_at = sys.argv[8]
+
+class TestHandler:
+     def __init__(self):
+          pass
+     def handle(self, added, removed):
+         pass
+
+
+# create num_sub kps subscribed at the triple t_sub
+t_sub = Triple(None,None,None)
+print "Creating the subscriptions..."
+if sub_at == "virtualsib":
+     ss = 0
+     for i in range(0, num_sub):
+          try:
+               kp_sub.append(SibLib(virtual_sib_ip, virtual_sib_port))
+               subs.append(kp_sub[i].create_rdf_subscription(t_sub, TestHandler()))
+          except:
+               print sys.exc_info()
+               
+          ss += 1
+     print "Started " + str(ss) + " subscriptions"
+
+elif sub_at == "realsib":
+     ss = 0
+     for i in range(0, num_sub):
+          try:
+               kp_sub.append(SibLib(sib_ip, sib_port))
+               subs.append(kp_sub[i].create_rdf_subscription(t_sub, TestHandler()))
+          except:
+               print sys.exc_info()
+          ss += 1
+     print "Started " + str(ss) + " subscriptions"
+
+#############################################################
+#
+# JOIN and CLEAN
+#
+#############################################################
+
+kp = []
+try:
+     rskp = SibLib(sib_ip, sib_port)
+     print "Created a kp for the insertions into the real sib"
+     kp.append(rskp)
+     lt = rskp.execute_rdf_query(Triple(None, None, None))
+     for t in lt:
+          rskp.remove(t)
+     print "Cleaned the real sib"
+
+     vskp = SibLib(virtual_sib_ip, virtual_sib_port)
+     print "Created a kp for the insertions into the virtual sib"
+     kp.append(vskp)
+
+except:
+     print sys.exc_info()
+#############################################################
+#
+# INSERT
+#
+#############################################################
+
+for client in [ rskp, vskp ]:
+    
+    if client == rskp:
+         print "Starting the insert into the real sib..."
+    else:
+         print "Starting the insert into the virtual sib..."
+    time.sleep(2)
+    
+    median_array = []
+    
+    # step iteration
+    for i in range(0, max_ins/step):
+        print "Inserting " + str((i+1) * step) + " triples at once"
+    
+        tot = []
+        for it in range(num_iter):   
+             
+            print '* [' + str((i+1) * step) + '] Iteration: ' + str(it)
+    
+            # generate (i+1)*step triples
+            t = []
+            for k in range(((i+1)*step)):
+                tt = Triple(URI(ns + "subject" + str(it) + "_" + str(k)), URI(ns + "predicate" + str(it)), URI(ns + "object" + str(it)))
+                t.append(tt)
+    
+            # insert the triples
+            try:
+                 end = timeit.timeit(lambda: client.insert(t), number=1)
+            except:
+                 print sys.exc_info()
+            
+            tot.append(end)
+    
+            # clean sib
+            try:
+                 lt = client.execute_rdf_query(Triple(None, None, None))
+                 for t in lt:
+                      client.remove(t)
+            except:
+                 print sys.exc_info()
+    
+        # sort the array and pick the median value
+        tot.sort()
+        if len(tot)%2 == 0:
+            median = (tot[len(tot)/2] + tot[len(tot)/2-1]) / 2
+        else:
+            median = tot[len(tot)/2]     
+            
+        median_array.append(round(median * 1000, 3))
+    
+    median_arrays.append(median_array)
+    
+    
+for i in median_arrays:
+    print "MEDIAN ARRAY: " + str(i)
+
+
+bar_chart = pygal.Bar(range=(0.0, 50.0))
+bar_chart.title = 'Insertion time with constant subscriptions varying the number of triples inserted'
+bar_chart.add('Local SIB', median_arrays[0])
+bar_chart.add('Remote SIB', median_arrays[1])
+bar_chart.render_to_file('multiple_insert.svg')
+
+
+#############################################################
+#
+# CLEAN
+#
+#############################################################
+
+print "NUM SUB: " + str(num_sub)
+
+ss = 0
+for i in range(0, num_sub):
+     try:
+          kp_sub[i].unsubscribe(subs[i])
+     except:
+          print sys.exc_info()
+     ss += 1
+    
+print str(ss) + " subscriptions closed"
+
+print "\nCleaning the sib...\n"
+try:
+    lt = rskp.execute_rdf_query(Triple(None, None, None))
+    for t in lt:
+         rskp.remove(t)
+except:
+     print sys.exc_info()
+
+print "Triples expected at this step: 0"
+print "Triples at this step: " + str(len(rskp.execute_rdf_query(Triple(None, None, None))))
+#vskp.leave_sib()
+
+try:
+     rskp.leave_sib()
+except:
+     print sys.exc_info()
