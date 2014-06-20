60555846bae2c5d83c74c52035d8a315ce70ce03
desmovalvo <desmovalvo@gmail.com>
2014-06-20 23:18:34 +0200

    Cleaned the xml_helpers.py file

--- virtualiser/lib/xml_helpers.py
+++ virtualiser/lib/xml_helpers.py
@@ -3,16 +3,25 @@
 from smart_m3.m3_kp import *
 import xml.etree.cElementTree as ET
 
-def extract_sparql_triples(doc):
+######################################################
+#
+# extract_sparql_triples
+#
+######################################################
+
+def extract_sparql_triples(doc_root):
+
+    """This function extracts all the triples from a sparql query response"""
+
     # Get the namespace
-    for parameter in doc.findall("parameter"):
+    for parameter in doc_root.findall("parameter"):
         if parameter.attrib["name"] == "results":
             sparql = parameter.getchildren()[0]
             namespace = sparql.tag[:-6]
     
     # The following code built the sparql result list
     rs = []
-    for result in doc.iter('{http://www.w3.org/2005/sparql-results#}result'):
+    for result in doc_root.iter('{http://www.w3.org/2005/sparql-results#}result'):
         r = []
         for binding in result.getchildren():
     
@@ -24,11 +33,18 @@ def extract_sparql_triples(doc):
     
     return rs
 
-# The following code built the sparql result list
-def extract_rdf_triples(doc):
+######################################################
+#
+# extract_sparql_triples
+#
+######################################################
+
+def extract_rdf_triples(doc_root):
+
+    """This function build the sparql result list"""
 
     result = []
-    for triple in doc.iter('triple_list'):
+    for triple in doc_root.iter('triple_list'):
         for field in triple.getchildren():
             tl = {}
             for f in field.getchildren():
@@ -46,14 +62,23 @@ def extract_rdf_triples(doc):
     return result 
 
 
-def build_dict(doc):
+######################################################
+#
+# build_dict
+#
+######################################################
+
+def build_dict(doc_root):
+    
+    """Function that creates a dictionary from a ssap message"""
+
     d = {} 
-    d["message_type"] = doc.find("message_type").text
-    d["transaction_type"] = doc.find("transaction_type").text
-    d["space_id"] = doc.find("space_id").text
-    d["node_id"] = doc.find("node_id").text
-    d["transaction_id"] = doc.find("transaction_id").text
-    for p in doc.findall("parameter"):
+    d["message_type"] = doc_root.find("message_type").text
+    d["transaction_type"] = doc_root.find("transaction_type").text
+    d["space_id"] = doc_root.find("space_id").text
+    d["node_id"] = doc_root.find("node_id").text
+    d["transaction_id"] = doc_root.find("transaction_id").text
+    for p in doc_root.findall("parameter"):
         if p.attrib["name"] == "status":
             d["status"] = p.text
         if p.attrib["name"] == "subscription_id":
